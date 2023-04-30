845d7dd40afd5c82aef898ff28dc24cd83adbe75
KhoiMe <j.m.tejada@hotmail.com>
2023-04-30 18:54:01 -0500

    feat: fugitive remapfeat: fugitive remapss

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,3 +1,10 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>")
 vim.keymap.set("n", "ge", "<cmd>diffget //3<CR>")
+
+local bufnr = vim.api.nvim_get_current_buf()
+local opts = { buffer = bufnr, remap = false }
+
+vim.keymap.set("n", "<leader>gp", function()
+    vim.cmd.Git('push')
+end, opts)
