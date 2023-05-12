d2d7b7a8363256dad773de22b5fd488d9bb309ca
KhoiMe <j.m.tejada@hotmail.com>
2023-05-12 08:43:10 -0500

    feat: Latex editing

--- after/plugin/vimtex.lua
+++ after/plugin/vimtex.lua
@@ -5,10 +5,10 @@ syntax enable
 
 let g:vimtex_view_method = 'zathura'
 
-let g:vimtex_view_general_viewer = 'okular'
+" let g:vimtex_view_general_viewer = 'okular'
 let g:vimtex_view_general_options = '--unique file:@pdf\#src:@line@tex'
 
-let g:vimtex_compiler_method = 'latexrun'
+let g:vimtex_compiler_method = 'latexmk'
 
 let maplocalleader = ","
 
