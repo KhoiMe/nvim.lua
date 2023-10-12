2442dd0708eee1e1f6f268f7fed931792ca2b62e
KhoiMe <j.m.tejada@hotmail.com>
2023-10-11 20:25:10 -0500

    weird

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -11,12 +11,23 @@ end)
 require('telescope').setup {
     defaults = {
     },
+    pickers = {
+        find_files = {
+            theme = "ivy",
+        },
+        oldfiles = {
+            theme = "ivy",
+        },
+        buffers = {
+            theme = "ivy",
+        },
+    },
     extensions = {
         fzf = {
-            fuzzy = true,               -- false will only do exact matching
+            fuzzy = true,                   -- false will only do exact matching
             override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-            case_mode = "smart_case",   -- or "ignore_case" or "respect_case"
+            override_file_sorter = true,    -- override the file sorter
+            case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
         },
     },
     planets = {
@@ -26,4 +37,3 @@ require('telescope').setup {
 }
 
 require('telescope').load_extension('fzf')
-
