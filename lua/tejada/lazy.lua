143f3aa5e16035cc3fd8dbc7a5d8ed6ca3c7750b
khoime <j.m.tejada@hotmail.com>
2025-09-26 22:53:35 -0500

    clean

--- lua/tejada/lazy.lua
+++ lua/tejada/lazy.lua
@@ -21,7 +21,6 @@ require("lazy").setup({
     { "blazkowolf/gruber-darker.nvim" },
     -- { 'tjdevries/colorbuddy.vim' },
     -- { 'tjdevries/gruvbuddy.nvim' },
-    -- { "catppuccin/nvim",          name = "catppuccin", priority = 1000 },
     -- { "ellisonleao/gruvbox.nvim", priority = 1000,     config = true,  opts = ... },
     -- { "rebelot/kanagawa.nvim" },
 
